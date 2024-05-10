ae224947174f5cacba9911b59e2af75e76d4113f
joychandrauday <joychandraud@gmail.com>
2024-05-10 09:00:49 +0600

    set up route to get the single book by id from database.

--- Index.js
+++ Index.js
@@ -36,6 +36,13 @@ async function run() {
       const result=await cursor.toArray();
       res.send(result)
     })
+    app.get('/book/:id',async(req,res)=>{
+      const id=req.params.id;
+      const query={_id: new ObjectId(id)}
+      const result= await bookCollection.findOne(query)
+      res.send(result)
+    })
+
     app.post('/allbooks',async(req,res)=>{
       const newBook=req.body;
       console.log(newBook)
